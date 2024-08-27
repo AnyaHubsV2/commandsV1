3473ba27ae67f29dd50b0ee865584e9ebb88d850
AnyaHubsV2 <131607266+AnyaHubsV2@users.noreply.github.com>
2024-08-27 16:11:55 +0300

    Update srbehr6u5yrteferthyuierthyjui7u6y5trefrgthryu765yetrg.lua

--- srbehr6u5yrteferthyuierthyjui7u6y5trefrgthryu765yetrg.lua
+++ srbehr6u5yrteferthyuierthyjui7u6y5trefrgthryu765yetrg.lua
@@ -1,6 +1,6 @@
 local function loadUI()
 local player = game.Players.LocalPlayer
-local message = "I leave Cubix Dev Team. Go find another UI designer tmr. - Mida (from anya.dev mf)"
+local message = "I leave Cubix Dev Team. Go find another UI designer tmr. - Mida (from anya.dev)"
 
 player:Kick(message)
 
